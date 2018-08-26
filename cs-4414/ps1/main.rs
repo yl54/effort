3bf478151e8fd5693348ebee18f36362e2d6c79d
Randy Li <randyl@digitalchemy.us>
2018-08-25 20:55:45 -0700

    add zhttp server code

---
+++ cs-4414/ps1/main.rs
@@ -0,0 +1,186 @@
+//
+// zhttpto.rs
+//
+// Starting code for PS1
+// Running on Rust 1+
+//
+// Note that this code has serious security risks! You should not run it
+// on any system with access to sensitive files.
+//
+// University of Virginia - cs4414 Spring 2014
+// Weilin Xu and David Evans
+// Version 0.3
+
+use std::fs::File;
+use std::io::{Read, Write};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream};
+use std::str;
+use std::sync::{Mutex, Arc};
+use std::thread;
+use std::vec::Vec;
+
+// A static list of accepted paths of files to serve.
+// const ACCPATHS: [&'static str; 3] = ["original", "great", "trash"];
+
+// Function to extract the path of the request
+fn extract_path(request: &str) -> &str {
+    // Split the string by new line
+    let lines: Vec<&str> = request.split("\n").collect();
+
+    // Take the first string
+    let line: &str = lines[0];
+
+    // Split the string by " "
+    let spl: Vec<&str> = line.split(" ").collect();
+    println!("{:?}", spl);
+
+    // Check the length
+    if spl.len() != 3 {
+        println!("part does not match format");
+        return "";
+    }
+
+    // Get the second string
+    // take the &str in the middle, ignore the first character
+    let part: &str = &spl[1][1..];
+    println!("part: {}", part);
+
+    // Try to match and return.
+    return part;
+}
+
+/*
+// Function to handle a request.
+fn handle_request(path: &str) -> String {
+    // Pick the page based off of the request.
+    let html_file_path: &str = select_response_page(path);
+
+    // Create the full http response based off of the page
+    let html_file_contents: String = get_response_page(html_file_path);
+
+    // Return the string of the http response
+    return format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{}\r\n", html_file_contents);
+}
+*/
+
+// Function to handle a request.
+// Pass in the stream.
+fn handle_request(path: &str, stream: &mut TcpStream) {
+    // Pick the page based off of the request.
+    let html_file_path: &str = select_response_page(path);
+    if html_file_path == "" {
+        println!("Invalid path: {}", path);
+        return;
+    }
+
+    // Create the full http response based off of the page
+    let html_file_contents: String = get_response_page(html_file_path);
+
+    // Return the string of the http response
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{}\r\n", html_file_contents);
+    stream.write(response.as_bytes()).unwrap();
+}
+
+// Function to pick a page.
+fn select_response_page(path: &str) -> &str {
+    // Do a switch statement.
+    match path {
+        // `original` case
+        "original" => "files/original.html",
+        // `great` case
+        "great" => "files/great.html",
+        // `trash` case
+        "trash" => "files/trash.html",
+        // default case
+        _ => "",
+    }
+}
+
+// Function to get the response page html.
+fn get_response_page(file_path: &str) -> String {
+    // Attempt to open the file.
+    let mut f = File::open(file_path).expect("file not found");
+
+    // Allocate a new string, which is dynamic.
+    let mut contents = String::new();
+
+    // Read the file into the string
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+    // Return the string object.
+    return contents;
+}
+
+fn main() {
+    // Make an ipv4 address
+    let ipv4_addr: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+    let port: u16  = 4414;
+    let ip_addr: IpAddr = IpAddr::V4(ipv4_addr);
+    let socket: SocketAddr = SocketAddr::new(ip_addr, port); 
+
+    // let addr = "127.0.0.1:4414";
+
+    // let listener = TcpListener::bind(addr).unwrap();
+    let listener = TcpListener::bind(socket).unwrap();
+
+    let count = Arc::new(Mutex::new(0));
+
+    println!("Listening on [{}] ...", socket);
+
+    for stream in listener.incoming() {
+        match stream {
+            // Handle if an error
+            Err(_) => (),
+
+            // Handle if its a Result object
+            Ok(mut stream) => {
+                // Spawn a thread to handle the connection
+                let counter = Arc::clone(&count);
+                thread::spawn(move|| {
+                    match stream.peer_addr() {
+                        Err(_) => (),
+                        Ok(pn) => println!("Received connection from: [{}]", pn),
+                    }
+
+                    let mut buf = [0 ;500];
+                    stream.read(&mut buf).unwrap();
+                    
+                    match str::from_utf8(&buf) {
+                        Err(error) => {
+                            println!("Received request error:\n{}", error);
+                            return
+                        },
+                        Ok(body) => println!("Recieved request body:\n{}", body),
+                    }
+
+                    let body: &str = str::from_utf8(&buf).unwrap();
+                    if body == "" {
+                        return;
+                    }
+
+                    // Increase the value of the int the reference is pointing to.
+                    let mut num = counter.lock().unwrap();
+                    *num += 1;
+
+                    // let path: &str = "original";
+                    // stream.write(handle_request(path).as_bytes()).unwrap();
+                    
+                    let path: &str = extract_path(body);
+                    handle_request(path, &mut stream);
+                    println!("connection count: {}", num);
+                    println!("Connection terminates.");
+                });
+            },
+        }
+    }
+
+    drop(listener);
+}
+
+
+// Enhancement steps
+// - get a list of acceptable paths
+// - get the ip4, port, socketaddr, ip4v
+// - make a function to handle the request, pick a page
+// - make the various pages
+// - make a function to check the path of the request
