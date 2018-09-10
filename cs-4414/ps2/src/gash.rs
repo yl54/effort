52fca4c0e183652eab9946005c9ba372e30ad5c0
Randy Li <randyl@digitalchemy.us>
2018-09-09 20:32:58 -0700

    cs-4414/ps2/: refactor command checks

--- cs-4414/ps2/src/gash.rs
+++ cs-4414/ps2/src/gash.rs
@@ -6,21 +6,33 @@ use std::env;
 use std::fmt::Display;
 use std::io::{self, Write};
 use std::process::Command;
+use std::thread;
 use std::vec::Vec;
+
+// Add a list of Gash actions: 1
+enum GashAction {
+    Continue,
+    Stop,
+    RunSync,
+    RunAsync,
+}
 
 pub struct Shell<'a> {
     cmd_prompt: &'a str,
     built_in:   BuiltIn,
+    // Add a cmd list for spawned commands: 1
+    // Add a threadpool here: 2
 }
 
 impl <'a>Shell<'a> {
     pub fn new(prompt_str: &'a str) -> Shell<'a> {
         Shell { 
             cmd_prompt: prompt_str,
-            built_in:   BuiltIn { history: Vec::new() },
+            built_in:   BuiltIn { history: Vec::new() },           
         }
     }
 
+    // run is the main loop for the gash shell.
     pub fn run(&mut self) {
         let stdin = io::stdin();
         let mut stdout = io::stdout();
@@ -38,34 +50,72 @@ impl <'a>Shell<'a> {
             println!("cmd_line: {}", cmd_line);
             println!("program: {}", program);
 
-            match program {
-                ""      =>  { continue; }
-                "exit"  =>  { return; }
-                _       =>  { self.validate_cmd(cmd_line) }
+            // Record every command that comes in.
+            self.built_in.record_cmd(program.to_string());
+
+            // Split the string by " "
+            let spl: Vec<&str> = cmd_line.split(" ").collect();
+
+            // Check how the input should be handled.
+            match self.check_cmd(program) {
+                GashAction::Continue => { continue; }
+                GashAction::Stop     => { return; }
+                GashAction::RunSync  => { 
+                    self.run_cmd(spl[0], program);
+                }
+                GashAction::RunAsync => { 
+                    // Push a copy of the string to a master list of strings
+
+                    // Make a thread
+
+                    self.run_cmd(spl[0], program);
+                }
             }
+
+            // 1: if it needs background process:
+            //    - add to spawned command list
+            //    - spawn a thread to run it
+            // 2: use a threadpool instead
         }
     }
 
-    // Maybe I need a validate_cmd to check if:
-    // - it can split into multiple strings
-    // - to run as a separate thread or on main process
-    fn validate_cmd(&mut self, cmd_line: &str) {
-        // Split the string by " "
+    // check_cmd checks the input and decides action to take.
+    fn check_cmd(&mut self, cmd_line: &str) -> GashAction {
+        match cmd_line {
+            ""      =>  { return GashAction::Continue; }
+            "exit"  =>  { return GashAction::Stop; }
+            _       =>  {  }
+        };
+
         let spl: Vec<&str> = cmd_line.split(" ").collect();
 
+        let len = spl.len();
+
         // Check if the array actually exists.
-        if spl.len() <= 0 {
+        if len <= 0 {
             println!("Input was not parsed into parts: {}", cmd_line);
-            return;
+            return GashAction::Continue;
         }
 
-        self.run_cmd(spl[0], cmd_line);
+        // Check if the command does not exist as a builtin or in the PATH.
+        if !self.built_in.is_built_in(spl[0]) && !self.path_cmd_exists(cmd_line) {
+            println!("Input was not a built in or on PATH: {}", cmd_line);
+            return GashAction::Continue;
+        }
 
-        self.built_in.record_cmd(cmd_line.to_string());
-
+        match spl[len - 1] {
+            "&" => { GashAction::RunAsync }
+            _ => { GashAction::RunSync }
+        }
     }
 
-    fn run_cmd(&mut self, cmd_name: &str, cmd_line: &str) {
+    // path_cmd_exists checks if the command exists on PATH.
+    fn path_cmd_exists(&self, cmd_path: &str) -> bool {
+        Command::new("which").arg(cmd_path).status().unwrap().success()
+    }
+
+    // run_cmd runs the command.
+    fn run_cmd(&self, cmd_name: &str, cmd_line: &str) {
         // Check if it is a builtin
         if self.built_in.is_built_in(cmd_name) {
             println!("BuiltIn command spotted from: {}", cmd_line);
@@ -81,6 +131,7 @@ impl <'a>Shell<'a> {
         }
     }
 
+    // run_custom_cmdline runs the custom command.
     pub fn run_custom_cmdline(&self, cmd_line: &str) {
         let argv: Vec<&str> = cmd_line.split(' ').filter_map(|x| {
             if x == "" {
@@ -91,20 +142,11 @@ impl <'a>Shell<'a> {
         }).collect();
 
         match argv.first() {
-            // Some(&program) => self.run_custom_cmd(program, &argv[1..]),
             Some(&program) => {
-                if self.cmd_exists(program) {
-                    io::stdout().write(&Command::new(program).args(&argv[1..]).output().unwrap().stdout).unwrap();
-                } else {
-                    println!("{}: command not found", program);
-                }
+                // At this point, its already been confirmed that the command exists on PATH.
+                io::stdout().write(&Command::new(program).args(&argv[1..]).output().unwrap().stdout).unwrap();
             },
-
             None => (),
         };
-    }
-
-    fn cmd_exists(&self, cmd_path: &str) -> bool {
-        Command::new("which").arg(cmd_path).status().unwrap().success()
-    }
+    }    
 }
