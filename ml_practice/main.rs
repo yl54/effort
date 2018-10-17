61797088c699f0139f05169db62c4370cdfbe599
Randy Li <randyl@digitalchemy.us>
2018-10-17 16:24:10 -0700

    ml_practice/: add iniital practice files

---
+++ ml_practice/main.rs
@@ -0,0 +1,18 @@
+/* 
+    This file is practicing basic ml math.
+    Rust is just to practice the language.
+*/
+
+#![allow(unused_mut)]
+#![allow(dead_code)] 
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unreachable_patterns)]
+#![allow(unused_assignments)]
+
+pub mod one_gate;
+
+// Main function
+fn main() {
+    one_gate::transform_example_simple();
+}
---
+++ ml_practice/one_gate.rs
@@ -0,0 +1,32 @@
+/* 
+    This file includes stuff to test a one gate transformer.
+*/
+
+// A transformer that does some stuff with inputs
+fn transform_multiply_gate(i: i16, j: i16) -> i16 {
+    // Return the multiplied value
+    return i * j;
+}
+
+// A function to do 1 gate manipulation
+fn transform_full_simple(i: i16, j: i16) -> i16 {
+    // Put the inputs through the transform gate
+    let output: i16 = transform_multiply_gate(i, j);
+
+    // Return the output
+    return output;
+} 
+
+// An example function to have its own inputs
+pub fn transform_example_simple() {
+    // Set input values
+    let input_1: i16 = 12;
+    let input_2: i16 = 3;
+
+    // Pass inputs into the gates
+    let output: i16 = transform_full_simple(input_1, input_2);
+
+    // Print out the result
+    println!("input: {}, {}", input_1, input_2);
+    println!("output: {}", output);
+}
