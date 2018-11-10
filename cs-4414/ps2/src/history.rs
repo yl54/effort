6579aff147ee61c73304f8e5cb541b61d2e96474
Randy Li <randyl@Unitys-MBP.home>
2018-11-09 20:43:25 -0800

    cs-4414/ps2/: deprecate history stuff

--- cs-4414/ps2/src/history.rs
+++ cs-4414/ps2/src/history.rs
@@ -4,45 +4,6 @@ use builtin::BuiltInCommand;
 use std::fmt::{Display, Formatter, Result};
 use std::string::String;
 
-// Struct to handle history
-pub struct History<'a> {
-    history: &'a Vec<String>,
-}
-
-impl <'a>History<'a> {
-    pub fn new(h: &'a Vec<String>) -> Self {
-        return History {
-            history: h,
-        };
-    }
-}
-
-// Implement BuiltInCommand for history
-impl<'a> BuiltInCommand for History <'a> {
-    // print
-    fn print(&self) {
-        println!("{:?}", self.history)
-    }
-
-    // run
-    fn run(&self) -> bool {
-        // Loop through each element in the history.
-        for record in self.history {
-            // Print the record in the history
-            println!("{}", record);
-        }
-
-        return true;
-    }
-}
-
-// Implement Display for history
-impl<'a> Display for History<'a> {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "History")
-    }
-}
-
 pub fn run(history: &Vec<String>) -> bool {
     for record in history {
         println!("{}", record);
