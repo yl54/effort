9983c8da91445db338fd73460327ac5bb1f82ec2
randyl <randyl@unity3d.com>
2019-01-06 15:17:16 -0800

    cs-4414/ps3/: refactor webserver stream handling into scheduler

--- cs-4414/ps3/src/server/webserver.rs
+++ cs-4414/ps3/src/server/webserver.rs
@@ -10,6 +10,7 @@ use std::sync::{Arc, Mutex};
 use httparse::{Error as HttpError, Request, Status, EMPTY_HEADER};
 
 use server::handlers;
+use server::scheduler::{Callback, Scheduler};
 use server::utils;
 
 const NUM_OF_HEADERS: usize = 30;
@@ -18,30 +19,18 @@ const NUM_OF_HEADERS: usize = 30;
 const SERVER_ADDR: &str = "127.0.0.1";
 const SERVER_PORT: &str = "20001";
 
-// Tip: For complex things like function parameters, denote with a type.
-type Callback = fn(&mut TcpStream);
-
-struct Handler {
-    // path is the url path.
-    path: String,
-
-    // handler is the function to execute.
-    handler: Callback,
-
-    // count is the number of times this handler has been requested.
-    count: Arc<Mutex<u16>>,
-}
-
 // Webserver 
 pub struct Webserver {
     // tcp_listener is the handle to the request listener.
     listener: TcpListener,
 
-    // handlers is a hashmap from the url path to handler functions.    
-    handlers: HashMap<String, Handler>,
+    // sc is a scheduler for each connection that comes to the Webserver.    
+    sc: Scheduler,
 
     // requests_total is the count of how many total requests have been recieved.
     requests_total: Arc<Mutex<u16>>,
+
+    
 }
 
 impl Webserver {
@@ -54,23 +43,14 @@ impl Webserver {
 
         return Webserver {
             listener: listener,
-            handlers: HashMap::new(),
+            sc: Scheduler::new(),
             requests_total: Arc::new(Mutex::new(0)),
         };
     }
 
     // register_handler registers a handler with a path.
     pub fn register_handler(&mut self, path: String, handler: Callback) {
-        let cl_path = path.clone();
-
-        let h = Handler {
-            path: path,
-            handler: handler,
-            count: Arc::new(Mutex::new(0)),
-        };
-
-        // Add the path + handler combination.
-        self.handlers.insert(cl_path, h);
+        self.sc.register_handler(path, handler);
     }
 
     // listen listens for requests and executes the proper handler if possible.
@@ -82,45 +62,9 @@ impl Webserver {
             *num_total += 1;
 
             match stream {
-                // Handle if an error.
                 Err(err) => debug!("Couldn't read the stream: {}", err.description()),
-
-                // Handle if its a Result object.
                 Ok(mut stream) => {
-                    // Read from the stream.
-                    // Q: Why do you have to read from the stream before stuff is written into it?
-                    let mut buf = [0 ;500];
-                    stream.read(&mut buf).unwrap();
-
-                    // Extract the body and path from the stream.
-                    let mut headers = [EMPTY_HEADER; NUM_OF_HEADERS];
-                    let mut req = Request::new(&mut headers[..]);
-                    let status = match req.parse(buf.as_ref()) {
-                        Ok(s) => s,
-                        Err(err) => {
-                            debug!("Failed parsing the bytes into a request.");
-                            continue
-                        },
-                    };
-
-                    let path = match req.path {
-                        Some(p) => p,
-                        None => {
-                            debug!("Failed getting the path from the request.");
-                            continue
-                        },
-                    };
-
-                    match self.handlers.get(&path.to_string()) {
-                        Some(h) => { 
-                            (h.handler)(&mut stream);
-                            let mut c = h.count.lock().unwrap();
-                            *c += 1;
-                        },
-                        None => {
-                            handlers::handle_default(&mut stream); 
-                        },
-                    }
+                    self.sc.schedule_stream(&mut stream);
                 }
             }
         }
