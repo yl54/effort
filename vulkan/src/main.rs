6eee3645014f56beac234f01aa70594d0de665a5
randyl <randyl@unity3d.com>
2019-05-11 17:13:15 -0700

    vulkan/: change location of window code

--- vulkan/src/main.rs
+++ vulkan/src/main.rs
@@ -27,57 +27,6 @@ fn run_app() {
     // q: where do i place this?
     let app = App{};
 
-    // Must keep in a loop to avoid just exiting immediately after 1 event
-    loop {
-        // Listen for events and handle event properly
-        // q: what does "|event|" mean? 
-        //    does it mean it moves the event into the closure? 
-        //    does it mean ownership transfer?
-        //    where does event come from?
-        // a: poll_events takes a function that references event struct
-        //    figure out how poll_events works
-        //    that function probably gets called and event is that thing
-        events_loop.poll_events(|event| {
-            // Check the type of event that has been detected
-            match event {
-
-                // Resized
-                Event::WindowEvent {
-                    event: WindowEvent::Resized(LogicalSize{width, height}),
-                    //q: what does "missing fields `window_id`, `event`" mean?
-                    // q: what is the point of ".."? 
-                    ..
-                } => {
-                    println!("Got a resized window event.")
-                },
-
-                // q: why do some events have fields that need to be with {}, and some can be wrapped around ()?
-                // KeyboardInput
-                Event::WindowEvent {
-                    // close requested
-                    event: WindowEvent::KeyboardInput{device_id, input},
-                    ..
-                } => {
-                    println!("Got a keyboard input window event.")
-                },
-
-                // Close Requested
-                Event::WindowEvent {
-                    // close requested
-                    event: WindowEvent::CloseRequested,
-                    ..
-                } => {
-                    println!("Got a close requested window event.")
-                },
-
-                // No event detected
-                _ => {
-                    ()
-                }
-            }
-        });
-    }
-
     // run the application
     // q: where does this go?
 }
--- vulkan/src/window.rs
+++ vulkan/src/window.rs
@@ -1,10 +1,19 @@
 // Window
 // check if window should be coupled with app or can be separate thing
 
+use winit::{
+    ControlFlow,
+    Event,
+    EventsLoop,
+    Window as WWindow,
+    WindowEvent
+};
+use winit::dpi::LogicalSize;
 
 // window struct
-// It will be based on the winit crate
 pub struct Window {
+    events_loop: EventsLoop,
+    window: WWindow,
 }
 
 // impl window
@@ -12,7 +21,68 @@ impl Window {
     // function to create a window
     // prob can be ported to a separate module
     pub fn default() -> Window {
+        let mut events_loop = EventsLoop::new();
+        let window = match WWindow::new(&events_loop){
+            Ok(w) => w,
+            Err(err) => { panic!("Failed to create window: {}", err) }
+        };
+
         Window {
+            events_loop,
+            window,
+        }
+    }
+
+    pub fn main_loop(&mut self) {
+        // Must keep in a loop to avoid just exiting immediately after 1 event
+        loop {
+            // Listen for events and handle event properly
+            // q: what does "|event|" mean? 
+            //    does it mean it moves the event into the closure? 
+            //    does it mean ownership transfer?
+            //    where does event come from?
+            // a: poll_events takes a function that references event struct
+            //    figure out how poll_events works
+            //    that function probably gets called and event is that thing
+            self.events_loop.poll_events(|event| {
+                // Check the type of event that has been detected
+                match event {
+
+                    // Resized
+                    Event::WindowEvent {
+                        event: WindowEvent::Resized(LogicalSize{width, height}),
+                        //q: what does "missing fields `window_id`, `event`" mean?
+                        // q: what is the point of ".."? 
+                        ..
+                    } => {
+                        println!("Got a resized window event.")
+                    },
+
+                    // q: why do some events have fields that need to be with {}, and some can be wrapped around ()?
+                    // KeyboardInput
+                    Event::WindowEvent {
+                        // close requested
+                        event: WindowEvent::KeyboardInput{device_id, input},
+                        ..
+                    } => {
+                        println!("Got a keyboard input window event.")
+                    },
+
+                    // Close Requested
+                    Event::WindowEvent {
+                        // close requested
+                        event: WindowEvent::CloseRequested,
+                        ..
+                    } => {
+                        println!("Got a close requested window event.")
+                    },
+
+                    // No event detected
+                    _ => {
+                        ()
+                    }
+                }
+            });
         }
     }
 }
