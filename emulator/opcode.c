33123503bca5c78ba7f536afb84d270ab0a6cecc
randyl <randyl@unity3d.com>
2019-08-12 16:07:42 -0700

    emulator/: add opcode converter skeleton

---
+++ emulator/opcode.c
@@ -0,0 +1,28 @@
+// This file will cover opcodes.
+// It will support the opcodes from the Intel 8080 microprocessor
+
+
+// imports
+
+// declarations
+
+// function to convert a buffer of hex to opcodes
+// q: does length of buffer have to be passed in as well?
+// q: what failure scenarios might occur in these steps?
+// q: do i return the coverted buffer, a success code, a pointer location, or nothing? 
+    // check if the buffer is valid
+
+    // while loop, check if pointer is pointing to buffer
+        // read the hex at that point
+
+        // check what type of hex it is
+        // long switch statement
+        // start with like 3 opcodes
+            // do stuff
+            // ...
+            // ...
+
+        // move pointer to next hex?
+
+    // return success code
+
