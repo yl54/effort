124c8dfb3c48df5914096abae20e3a9a3e958a1a
randyl <randyl@unity3d.com>
2019-08-13 16:28:20 -0700

    emulator/: add function to read hex at pc pointer

--- emulator/opcode.c
+++ emulator/opcode.c
@@ -6,23 +6,36 @@
 
 // declarations
 
-// function to convert a buffer of hex to opcodes
 // q: does length of buffer have to be passed in as well?
 // q: what failure scenarios might occur in these steps?
-// q: do i return the coverted buffer, a success code, a pointer location, or nothing? 
-    // check if the buffer is valid
+// q: do i return the coverted buffer, a success code, a pointer location, or nothing?
+
+
+// disassemble_8080 converts a buffer of hex codes to a buffer of 8080 opcodes
+// * input: a buffer of hex codes to convert. use unsigned char b/c its range is 0 - 255. Each char is one hex value.
+int disassemble_buffer_into_8080(unsigned char* input) {
+    // initialize a pc to count through everything
 
     // while loop, check if pointer is pointing to buffer
-        // read the hex at that point
+        // pass into dissasemble hex function
 
-        // check what type of hex it is
-        // long switch statement
-        // start with like 3 opcodes
-            // do stuff
-            // ...
-            // ...
-
-        // move pointer to next hex?
+        // move pointer to next hex value
 
     // return success code
+    return 0;
+}
 
+// function to disassemble one hex
+// * input: a buffer of hex codes to convert. use unsigned char b/c its range is 0 - 255. Each char is one hex value.
+// * pc   : a program counter to point to a hex value to analyze
+int disassemble_hex_into_8080(unsigned char* input, int pc) {
+    // switch case
+    // do something with opcode
+    // start with println
+        // do stuff
+        // ..
+        // ..
+
+    // return success code
+    return 0;
+}
