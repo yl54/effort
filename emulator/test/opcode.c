9acead747b90a5ffd63cd7bb974f5b140b3ddc60
randyl <randyl@unity3d.com>
2019-08-14 15:54:01 -0700

    emulator/: add initial test code for opcode code

---
+++ emulator/test/opcode.c
@@ -0,0 +1,18 @@
+// This file will test the opcode functions
+
+// include
+// q: is there a testing framework for c?
+
+// declared functions
+
+// test opcode happy path
+    // Get a file of hex to read in.
+    // Get some hex variable to read in.
+
+    // Pass the hex instructions into a buffer.
+
+    // Pass the hex instructions into the disassemble function
+
+    // assert that the result is not an error
+
+// test specific inputs get specific outputs
