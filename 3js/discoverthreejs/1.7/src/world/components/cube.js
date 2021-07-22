c3d25de7f4c00b21d47d57a0caa5db7265d1cad2
randyl <randyl@unity3d.com>
2021-07-22 16:13:14 -0700

    add 1.7 initial setup

---
+++ 3js/discoverthreejs/1.7/src/world/components/cube.js
@@ -0,0 +1,26 @@
+// import
+import { BoxBufferGeometry, Mesh, MeshStandardMaterial } from '../../.././vendor/three/build/three.module.js';
+
+const spec = {
+	color: 'purple',
+}
+
+// create function
+function createCube() {
+	// Create a geometry
+	const geometry = new BoxBufferGeometry(2, 2, 2);
+
+	// Create a MeshStandardMaterial
+	const material = new MeshStandardMaterial(spec);
+
+	// Create a Mesh 
+	const cube = new Mesh(geometry, material);
+
+	cube.rotation.set(-0.5, -0.1, 0.8);
+
+	// return the mesh. its not a cube anymore
+	return cube;
+}
+
+// export
+export { createCube };
