c8fb3fd3e6bc2b0ca686707af55228908cbc6a92
randyl <randyl@unity3d.com>
2021-08-30 12:07:38 -0700

    3js 1.3, add challenges

--- 3js/discoverthreejs/1.3/src/main.js
+++ 3js/discoverthreejs/1.3/src/main.js
@@ -3,14 +3,32 @@ import { World } from './world/world.js';
 
 // main function
 function main() {
-	// Get a reference to the container element
+	// Get a reference to the html elements, and keep them hidden to begin
 	const container = document.querySelector('#scene-container');
+	container.style.visibility = "hidden";
+	const heading = document.querySelector('#heading');
+	heading.style.visibility = "hidden";
 
 	// Create an instance of the world app
 	const world = new World(container);
 
-	// Render the scene
-	world.render();
+	// Challenge Hard: 1
+	// Add a button trigger the rendering of the scene
+	const button = document.querySelector('#render-button');
+
+	button.addEventListener('click', () => {
+		// Render the scene after the click occurs
+		world.render();
+
+		console.log("clicked");
+
+		// Show the elements once clicked
+		heading.style.visibility = "visible";
+		container.style.visibility = "visible";
+		button.style.visibility = "hidden";
+		button.style.disabled = true;
+	});
+
 }
 
 // call the main function
